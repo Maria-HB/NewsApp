50d9331d5f4c2da367a7acf7ee5e0134dc624c26
Maria-HB <mariahabib@gmail.com>
2021-10-28 17:54:17 +1100

    Main storyboard removed
    Main storyboard removed from project and window initialised programmatically inside scene delegate.

--- NewsApp/SceneDelegate.swift
+++ NewsApp/SceneDelegate.swift
@@ -13,10 +13,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        //get the window scene
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        //create a new window using the window scene constructor
+        let window = UIWindow(windowScene: windowScene)
+        
+        //create your root view controller and add it to a navigation controller
+        let newsListViewController = NewsListViewController()
+        let navigationController = UINavigationController(rootViewController: newsListViewController)
+        
+        //add the navigation controller to the window's root view controller
+        window.rootViewController = navigationController
+        
+        //make the window visible
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
